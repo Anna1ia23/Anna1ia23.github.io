bbe7a719e364bd4a9bacf7274801169a2b66058c
Katie T <kttackett123@gmail.com>
2023-04-13 15:28:26 -0400

    image update

--- JS_Files/search.js
+++ JS_Files/search.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     loginForm.addEventListener("submit", e => {
         //store user's searched terms in a const/global variable?
         //check if said terms are in the array here
-        //if they are, go to results page. if not, noresultsfound page?
+        //if they are, go to results page. if not, idk do something
     });
 
 });
