cb7e90ef063a9aa4dd293be429ffb183a76d1b89
AlexanderOr <aortwine@ltu.edu>
2023-04-19 15:44:36 -0400

    back end script
    Added the code for the back end php...trying to look through it to make sense of it but wanted to get it to you guys to see if you could get anything out of it.
    
    I have the log in script on the html page just in case this proves to be hard to implement.

---
+++ Backend-script.php
@@ -0,0 +1,66 @@
+<?php
+    include_once'FALC/connectionScript.php';
+    
+    //THIS IS ALL OF THE WEBSITE,TRYING TO GROOM THROUGH IT TO MAKE SENSE OF HALF OF IT
+
+    /*
+    $db=$conn;
+
+    function fetch_data()
+    {
+        global $db;
+        $query = "SELECT * from usertable ORDER BY id DESC";
+        $exec = mysqli_query($db, $query);
+        if(mysqli_num_rows($exec)>0)
+        {
+            $row=mysqli_fetch_all($exec, MYSQLI_ASSOC);
+            return $row
+
+        }
+        else{
+            return $row= [];
+        }
+    }
+
+    $fetchData= fetch_data();
+
+    show_data($fetchData);
+    
+    function show_data($fetchData){
+        echo '<table border="1">
+        <tr>
+            <th>S.N</th>
+            <th>Full Name</th>
+            <th>Email Address</th>
+            <th>City</th>
+            <th>Country</th>
+            <th>Edit</th>
+            <th>Delete</th>
+        </tr>';
+        if(count($fetchData)>0){
+             $sn=1;
+             foreach($fetchData as $data){ 
+             echo "<tr>
+                 <td>".$sn."</td>
+                 <td>".$data['fullName']."</td>
+                 <td>".$data['emailAddress']."</td>
+                 <td>".$data['city']."</td>
+                 <td>".$data['country']."</td>
+                 <td><a href='crud-form.php?edit=".$data['id']."'>Edit</a></td>
+                 <td><a href='crud-form.php?delete=".$data['id']."'>Delete</a></td>
+                 </tr>";
+                            
+             $sn++; 
+         }
+    }
+    else{
+     
+         echo "<tr>
+         <td colspan='7'>No Data Found</td>
+         </tr>"; 
+    }
+    echo "</table>";
+}
+
+    */
+?>
