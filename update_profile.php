eb9b5afd88c5f708805be87ffdee2585f9f92cfb
Anna1ia23 <128739828+Anna1ia23@users.noreply.github.com>
2023-04-20 10:12:54 -0400

    Add files via upload
    file to process user information from EditProfile.html

---
+++ update_profile.php
@@ -0,0 +1,33 @@
+<?php
+//This is embedded php that will update the database with the users information
+//Connects to the database
+include_once'Anna1ia23.github.io/connectionScript.php';
+
+//Check if form submitted
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+  //Get user input
+    $name = $_POST['userName'];
+    $instagram = $_POST['userInstagram'];
+    $linkedin = $_POST['userLinkedIn'];
+    $canvas = $_POST['userCanvas'];
+
+
+    try{
+    //Insert user information into database
+    $sql = "INSERT INTO [dbo].intern_profile (intern_name, instagram_username, linkedin_username, canvas_username) VALUES (?, ?, ?, ?)";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$name, $instagram, $linkedin, $canvas]);
+
+    echo "Profile Successfully Updated!";
+
+    //Redirects to home page
+    header('Location: index.html');
+    exit;
+    }
+    //if there is an issue updating the DB, the catch block will execute an error statement
+    catch(PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
+}
+?>
