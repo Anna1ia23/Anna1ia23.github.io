85e4e7151231d05defd421a33e7d7f23175f68f3
AlexanderOr <aortwine@ltu.edu>
2023-04-20 13:46:54 -0400

    login setup
    script that I think will work for the login page. just need to connect it once we figure out why the signup page isn't connecting to the back end script.

---
+++ PHP_Files/LogInBackEnd.php
@@ -0,0 +1,30 @@
+<?php
+    include_once'FALC/connectionScript.php';
+
+    //input from user should be saved to a username and password variable. These are example variables in php
+    //but I think you can use javascript to fill them.
+    $username = $_POST['logInUsername']
+    $password = $_POST['logInPassword']
+
+
+
+    //this is the sql statement that will check if there is a record with both username and password
+    $sql="SELECT * FROM user_login WHERE username='$givenUsername' AND password='$givenPassword';"
+
+    //This executes the above statement and saves it into a 'result' variable:
+    $result=$conn->query($sql);
+
+    //this will check if there is a match:
+    if($result->num_rows >0){
+        echo "Access granted. ";
+        //THIS IS WHERE THEY SHOULD BE ALLOWED ACCESS TO THEIR ACCOUNT/SEARCH PAGE
+    }
+    else{
+         echo "Invalid username or password. Please try again. If new user, please sign up.";
+         header('Location: /SignUpLogIn.html');
+         exit;
+     }
+
+
+
+?>
