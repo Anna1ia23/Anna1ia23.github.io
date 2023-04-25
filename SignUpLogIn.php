e92f8b43726f5a868dc98e3ffe8ccc6aeb1f4b4b
AlexanderOr <aortwine@ltu.edu>
2023-04-25 15:41:39 -0400

    Update SignUpLogIn.php

--- SignUpLogIn.php
+++ SignUpLogIn.php
@@ -62,13 +62,13 @@
 
                     //input from user should be saved to a username and password variable. These are example variables in php
                     //but I think you can use javascript to fill them.
-                    $username = $_POST['email']
-                    $password = $_POST['password']
+                    $username = $_POST['email'];
+                    $password = $_POST['password'];
 
 
 
                     //this is the sql statement that will check if there is a record with both username and password
-                    $sql="SELECT * FROM user_login WHERE username='$givenUsername' AND password='$givenPassword';"
+                    $sql="SELECT * FROM user_login WHERE username='$givenUsername' AND password='$givenPassword';";
 
                     //This executes the above statement and saves it into a 'result' variable:
                     $result=$conn->query($sql);
